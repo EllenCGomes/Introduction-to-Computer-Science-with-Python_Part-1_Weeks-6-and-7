a127a76411bfbe63e3a6524a37bf7d2c96bcf47a
Ellen Gomes <78104233+EllenCGomes@users.noreply.github.com>
2022-04-01 15:52:59 -0300

    Update EX05 - sum_hypotenuses.py

--- EX05 - sum_hypotenuses.py
+++ EX05 - sum_hypotenuses.py
@@ -6,9 +6,9 @@ def sum_hypotenuses(x):
         while adjacent <= x:
             n = ((opposite**2) + (adjacent**2))**(1/2) #calculate hypotenuse 
             if n-int(n) == 0 and n <= x: #check if the number is integer and less than x
-                sum = sum + n
-            adjacent = adjacent + 1
-        opposite = opposite + 1
+                sum += n
+            adjacent += 1
+        opposite += 1
         adjacent = opposite #avoid duplicate (ex: op = 2; ad = 1 e op = 1; ad = 2)
     return (sum)    
 
